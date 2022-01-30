1efe2e09281dd28a1615359fb44d8316e5c5c7b6
FrontWorksDev <kengo.kojima@frontworks.dev>
2022-01-30 13:24:56 +0900

    feat: create update & delete modal test

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,15 +10,13 @@ import DeleteIcon from "@mui/icons-material/Delete";
 type Props = {
   handleDelete: Function;
   handleUpdate: Function;
-  slug: string;
   ID: number;
   title: string;
 };
 
-function TaskList({ handleDelete, handleUpdate, slug, ID, title }: Props) {
+function TaskList({ handleDelete, handleUpdate, ID, title }: Props) {
   return (
     <ListItem
-      key={slug}
       secondaryAction={
         <IconButton
           edge="end"
